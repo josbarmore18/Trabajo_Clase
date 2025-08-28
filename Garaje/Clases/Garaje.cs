0f62e8975151c28ba589a91e15b62c97c552b9a8
JosueBarahona <202000365@umh.edu.hn>
2025-08-27 21:19:10 -0600

    Trabajo de clase

---
+++ Garaje/Clases/Garaje.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaje.Clases
+{
+    public class Garaje
+    {
+        // Atributos
+        public int numeroGaraje { get; set; }
+        public string descripcion { get; set; }
+        public int capacidadEspacios { get; set; }
+        public int espaciosOcupados { get; set; }
+
+        // Constructor para inicializar todas las variables
+        public Garaje(int numeroGaraje, string descripcion, int capacidadEspacios, int espaciosOcupados)
+        {
+            this.numeroGaraje = numeroGaraje;
+            this.descripcion = descripcion;
+            this.capacidadEspacios = capacidadEspacios;
+            this.espaciosOcupados = espaciosOcupados;
+        }
+
+        // Método para retornar el número de espacios disponibles
+        public int getEspaciosDisponibles()
+        {
+            return capacidadEspacios - espaciosOcupados;
+        }
+
+        // Método para actualizar los espacios ocupados
+        public void setEspaciosOcupados(int espaciosSolicitados)
+        {
+            this.espaciosOcupados = this.espaciosOcupados + espaciosSolicitados;
+        }
+    }
+}
---
+++ Garaje/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Garaje.Clases; // Agrega esta línea para importar la carpeta Clases
+
+namespace Garaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ahora se puede usar 'Garaje' sin conflicto
+            Clases.Garaje miGaraje = new Clases.Garaje(1, "Garaje Principal", 50, 20);
+
+            Console.WriteLine($"Número de garaje: {miGaraje.numeroGaraje}");
+            Console.WriteLine($"Descripción: {miGaraje.descripcion}");
+            Console.WriteLine($"Capacidad total: {miGaraje.capacidadEspacios}");
+            Console.WriteLine($"Espacios ocupados: {miGaraje.espaciosOcupados}");
+
+            // Muestra los espacios disponibles
+            Console.WriteLine($"\nEspacios disponibles: {miGaraje.getEspaciosDisponibles()}");
+
+            // Actualiza los espacios ocupados
+            int espaciosASolicitar = 5;
+            Console.WriteLine($"\nSolicitando {espaciosASolicitar} nuevos espacios...");
+            miGaraje.setEspaciosOcupados(espaciosASolicitar);
+
+            // Vuelve a mostrar los espacios ocupados y disponibles
+            Console.WriteLine($"\nEspacios ocupados ahora: {miGaraje.espaciosOcupados}");
+            Console.WriteLine($"Espacios disponibles ahora: {miGaraje.getEspaciosDisponibles()}");
+
+            Console.ReadLine();
+        }
+    }
+}
